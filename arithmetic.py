f169f3fb0112f9421a50d514bf28c68461e358ca
marksantiago1 <115474677+marksantiago1@users.noreply.github.com>
2023-07-06 15:08:49 +0800

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+num1 = 5
+num2 = 8
+
+sum = num1 + num2
+print("Sum = {0}".format(sum))
+
+diff = num1 - num2
+print("Difference = {0}".format(diff))
+
+product = num1 * num2
+print("Product = {0}".format(product))
+
+quotient = num1 / num2
+print("Quotient = {0}".format(quotient))
---
+++ main.py
@@ -0,0 +1,28 @@
+print("Programmer: Mark H. Santiago")
+
+print("Date Created: July 6, 2023")
+
+print("Description of the Program: Display the following information")
+
+print("First Name: Mark ")
+
+print("Middle Name: Habuchi ")
+
+print("Last Name: Santiago ")
+
+print("Course: Bachelor of Science in Information Technology")
+
+print("Year: 3rd year ")
+
+print("Age: 21 ")
+
+print("Gender: Male ")
+
+print("Civil Status: Single ")
+
+print("Nationality: Filipino ")
+
+print("Motto: ")
+
+print("School: PHINMA Araullo University")
+print("College: CITE")
